ae4404fe553b0d8c178780ac93bce07d3d182064
Ahmed Wael <waely@std.mans.edu.eg>
2024-12-15 15:50:26 +0200

    process struct

---
+++ WHASOS/WHASOS/process.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+struct process
+{
+    long burstTime;
+    long arrivalTime;
+    string processID;
+    long startTime;
+    long endTime;
+    long waitTime;
+    int lastRemainingBurst;
+    int pBarValue = 0;
+    int priority;
+	process() {};
+    process(string processID, long arrivalTime, long burstTime, long startTime);
+    process(string processID, long arrivalTime, long burstTime, long startTime, long priority);
+    // default constructor
+};
+
